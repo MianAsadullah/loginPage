fc8eebb7af6ee2afae235f83f644990424f5653d
assad <mianasadullah978@gmail.com>
2021-01-06 20:07:38 +0500

    done

--- src/components/persons/persons.js
+++ src/components/persons/persons.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from "react";
 
- const persons = (props) => 
-(return()
-    );
-    export default persons
+const persons = (props) =>
+  props.persons.map((person, index) => {
+    <Person
+      click={() => this.deletePersonHandler(index)}
+      name={person.name}
+      age={person.age}
+      key={person.id}
+      changeName={(event) => this.nameChangeHandler(event, person.id)}
+      changeAge={(event) => this.ageChangeHandler(event, person.id)}
+    />;
+  });
+
+export default persons;
